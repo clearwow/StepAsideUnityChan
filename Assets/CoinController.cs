56895593bf920d15444c2908a8d80235e58e7f6f
keijun12 <73667918+keijun12@users.noreply.github.com>
2020-12-07 22:22:14 +0900

    Initial commit

---
+++ Assets/CoinController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class CoinController : MonoBehaviour
+{
+    private GameObject unitychan;
+
+    // Use this for initialization
+    void Start()
+    {
+        //回転を開始する角度を設定
+        this.transform.Rotate(0, Random.Range(0, 360), 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //回転
+        this.transform.Rotate(0, 3, 0);
+
+        this.unitychan = GameObject.Find("unitychan");
+        if(this.transform.position.z < this.unitychan.transform.position.z-10)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/ConeController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConeController : MonoBehaviour
+{
+    private GameObject unitychan;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.unitychan = GameObject.Find("unitychan");
+        if (this.transform.position.z < this.unitychan.transform.position.z - 5)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
